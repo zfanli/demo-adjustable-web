333353928e9eee90303fc3537ec6002208c4f545
Richard <zfanli@outlook.com>
2019-06-19 18:06:09 +0800

    update: add main view

---
+++ src/views/AdjustableView.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const AdjustableView: React.FC = () => {
+  return (
+    <div className="adjustable-view">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default AdjustableView;
