9bcec10d89eb9a075fc7de6076cb03197594cae4
Richard <zfanli@outlook.com>
2019-06-20 01:13:19 +0800

    fix: 404 handler always shows

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AdjustableView from './views/AdjustableView';
 import NotFound from './views/NotFound';
 import './css/common.scss';
@@ -8,8 +8,10 @@ import './css/common.scss';
 const App: React.FC = () => {
   return (
     <Router>
-      <Route path="/" exact component={AdjustableView} />
-      <Route component={NotFound} />
+      <Switch>
+        <Route path="/" exact component={AdjustableView} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 };
