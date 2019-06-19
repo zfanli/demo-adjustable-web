a4c200365ab3ce39f067d3cf472f1e47a254f5bd
Richard <zfanli@outlook.com>
2019-06-19 16:33:08 +0800

    update: locales as a module

---
+++ src/locales/index.ts
@@ -0,0 +1,6 @@
+import en from './en.json';
+import jp from './jp.json';
+import { Locale } from '../type.js';
+
+// export language strings
+export const locales: { [k: string]: Locale } = { en, jp };
