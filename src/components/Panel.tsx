1b387611e5e440244c45376770c7efc87d028780
Richard <zfanli@outlook.com>
2019-06-20 18:15:28 +0800

    update: build basic framwork

---
+++ src/components/Panel.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Panel: React.FC = () => {
+  return <section>Panel</section>;
+};
+
+export default Panel;
--- src/views/AdjustableView.tsx
+++ src/views/AdjustableView.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Panel from '../components/Panel';
+
+import '../css/adjustableView.scss';
 
 const AdjustableView: React.FC = () => {
+  const panelConfig = new Array(5).fill(0);
+
   return (
     <>
       <Header />
+      <div className="av-content">
+        {panelConfig.map(() => (
+          <Panel />
+        ))}
+      </div>
       <Footer />
     </>
   );
