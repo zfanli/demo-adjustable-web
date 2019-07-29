c6286716b364b85c5fd12b3f3a2f51e7b914d078
Richard <zfanli@outlook.com>
2019-07-30 00:18:37 +0800

    fix: compatibility of IE11

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
+// These must be the first lines in src/index.js
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './reducers'
 import * as serviceWorker from './serviceWorker'
-
-// For compatible.
-import 'babel-polyfill'
 
 ReactDOM.render(
   <Provider store={store}>
