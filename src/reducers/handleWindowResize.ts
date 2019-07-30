f51644a00e7ba269f8129c439985de638f7dccdd
Richard <zfanli@outlook.com>
2019-07-30 15:59:20 +0800

    update: check border while resize window

--- src/reducers/handleWindowResize.ts
+++ src/reducers/handleWindowResize.ts
@@ -1,4 +1,4 @@
-import { State, BaseAction, SingleReducer } from '../type'
+import { State, BaseAction, SingleReducer, PanelWithPosition } from '../type'
 import { HANDLE_WINDOW_RESIZE } from '../actions'
 import {
   handleSizeChangeForSortable,
@@ -8,7 +8,7 @@ import {
 const handleWindowResize = (state: State, action: BaseAction): State => {
   const containerSize = action.payload.size
   // Change relative positions and sizes.
-  const [resizePanels, resizeOrder] = state.settings.sortable
+  const panels = state.settings.sortable
     ? handleSizeChangeForSortable(
         state.panels,
         state.order,
@@ -22,14 +22,27 @@ const handleWindowResize = (state: State, action: BaseAction): State => {
         containerSize,
         state.settings.containerSize
       )
+
+  panels.forEach((panels: PanelWithPosition[]) =>
+    panels.forEach(p => {
+      const maxLeft = state.settings.containerSize.width - p.width
+      const minLeft = state.settings.margin
+      const maxTop = state.settings.containerSize.height - p.height
+      const minTop = state.settings.margin
+
+      p.left = p.left < minLeft ? minLeft : p.left > maxLeft ? maxLeft : p.left
+      p.top = p.top < minTop ? minTop : p.top > maxTop ? maxTop : p.top
+    })
+  )
+
   // const tempSettings
   return Object.assign({}, state, {
     settings: {
       ...state.settings,
       containerSize,
     },
-    panels: resizePanels,
-    order: resizeOrder,
+    panels: panels[0],
+    order: panels[1],
   })
 }
 
